8bf01ea808d5db857f7079363fd87de4e4d1f469
Az1402 <atharvrzalte@gmail.com>
2024-10-28 16:45:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Paste from './components/Paste'
+import Viewpaste from './components/Viewpaste'
+
+const router = createBrowserRouter(
+  [
+    {
+      path :"/",
+      element : 
+      <div>
+        <Navbar/>
+        <Home/>
+      </div>
+    },
+
+    {
+      path :"/pastes",
+      element :
+       <div>
+        <Navbar/>
+        <Paste/>
+      </div>
+    },
+
+    {
+      path :"/pastes/:id",
+      element : 
+      <div>
+        <Navbar/>
+        <Viewpaste/>
+      </div>
+    },
+  ]
+  
+)
+
+function App() {
+  
+
+  return (
+    
+    <RouterProvider router={router}/>
+      
+    
+  )
+}
+
+export default App
